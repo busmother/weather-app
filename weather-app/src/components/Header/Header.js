5f346adb092d3069e0452b5481a49d58f36047d5
Ian Marshall <imarshall14@gmail.com>
2021-08-28 17:19:27 -0400

    add Header.js

---
+++ weather-app/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Logo from '../../elements/Logo/Logo.js'
+
+const header = (props) => {
+    return(
+        <header className='header'>
+            <Logo colorScheme='light'/>
+        </header>
+    );
+}
+
+export default header;
